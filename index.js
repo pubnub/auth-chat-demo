5586de81a48b7bac00b7ac6f6440cb8a1d1578ad
Tomomi Imura <tomomi.i@gmail.com>
2015-07-14 14:29:00 -0700

    add console.log

--- index.js
+++ index.js
@@ -136,6 +136,8 @@
       res.redirect('/');
   });
 
-  var server = app.listen(process.env.PORT | 3000);
+  var server = app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 })();
